6379646eddb66e49cad3717a6d8d5abe27848190
lejlailles <il970322@gmail.com>
2022-03-11 15:17:48 +0100

    lejla neve

--- src/javagitproba/JavaGitProba.java
+++ src/javagitproba/JavaGitProba.java
@@ -4,7 +4,7 @@ public class JavaGitProba {
     
     public static void main(String[] args) {
         
-        String nev1;
+        String nev1 = "Illés Lejla";
         String nev2 = "Hecz Klaudia";
         String nev3;
     }
