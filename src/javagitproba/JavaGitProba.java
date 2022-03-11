04659302ef6558b1b973a972f9d3ebabd2c03af3
lejlailles <78789126+lejlailles@users.noreply.github.com>
2022-03-11 15:10:52 +0100

    Nevek

--- src/javagitproba/JavaGitProba.java
+++ src/javagitproba/JavaGitProba.java
@@ -4,7 +4,9 @@ public class JavaGitProba {
     
     public static void main(String[] args) {
         
-        String nev1, nev2, nev3;
+        String nev1;
+        String nev2;
+        String nev3;
     }
     
 }
